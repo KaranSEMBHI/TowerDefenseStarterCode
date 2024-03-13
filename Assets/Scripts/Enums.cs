697b068b3b96f545e2a0ceadc62845651922f591
KaranSEMBHI <Karan186en186@gmail.com>
2024-03-13 19:42:46 +0100

    Stap 2
    Alles uitgewerkt wat u vroeg.

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum Path
-
 {
-
     Path1,
-
     Path2
-
+}
+public enum TowerType
+{
+    Archer,
+    Sword,
+    Wizard
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    public int damage;
+
+    void Start()
+    {
+        // Rotate the projectile towards the target
+        if (target != null)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        }
+    }
+
+    void Update()
+    {
+        // When target is null, it no longer exists and this  
+        // object has to be removed
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // Move the projectile towards the target
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        // Check if the distance between this object and
+        // the target is smaller than 0.2. If so, destroy this object.
+        if (Vector3.Distance(transform.position, target.position) < 0.2f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
